0964903034d503089b8f9cfefe9b553abeb152af
Kolton Starr <starrkolton@gmail.com>
2019-05-01 19:24:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  count = 0 
+  nuarray = []
+  while count < arr.length 
+    x = yield(arr[count])
+    nuarray << x
+  
+    count += 1 
+  end
+  "[#{nuarray[0]}" + ", " "#{nuarray[1]}]"
 end
